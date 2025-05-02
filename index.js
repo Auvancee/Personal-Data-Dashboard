454c5dae2723d991dcc1ebab10d5cda5742b3017
Aakif Umar <aakifumar55@gmail.com>
2025-05-02 12:30:23 -0700

    - Added test usage of writeDb with sample data object
    - Demonstrated writing to JSON database using
    writeDb
    
    - Demonstrated reading from JSON database using
    readDb
    
    - Modified existing database object and
    persisted updated data
    
    - Simulated real-world usage by updating
    and rewriting stored JSON

---
+++ index.js
@@ -0,0 +1,20 @@
+const { write } = require("fs");
+const { readDb, writeDb } = require("./db_functions");
+
+// (1)
+// const dataObj = {
+//     name: 'Aakif',
+//     favoriteGames: [8, 3]
+// }
+
+// writeDb(dataObj)
+
+// (2)
+// console.log(readDb())
+
+
+
+// (3)
+// const savedData = readDb()
+// savedData['NumberOfCars'] = 50
+// writeDb(savedData)
